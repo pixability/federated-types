59873648e68fca9569f641e906e09952409a260b
Jonathan Greene <jon@greenethumb.com>
2020-11-02 11:10:18 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,75 @@
+## What is this for?
+
+With Webpack 5's ModuleFederationPlugin, module federation is easy to implement. Coupling it with a mono-repo is powerful, but if your project uses Typescript, it's tedious to manually create/maintain ambient type definitions for your packages so Typescript can resolve the dynamic imports to their proper types. While using @ts-ignore on your imports works, it is a bummer to lose intellisense and type-checking capabilities.
+
+This package exposes a node CLI command called `make-federated-types`. Once installed, you can run that command within a package, and it will write a typings file for your package into you node_modules directory that will be resolved by the Typescript compiler.
+
+## How is this used?
+
+You'll need to install this module with either NPM or yarn:
+
+```
+yarn add @pixability-ui/federated-types
+```
+
+You'll also need to place a `federation.config.json` in each package being federated. It will contain the remote name and exported members. These properties are used in webpoack's `ModuleFederationPlugin` configuration object. An exmaple:
+
+#### `federation.config.json`
+
+```json
+{
+    "name": "app2",
+    "exposes": {
+        "./Button": "./app2/Button"
+    }
+}
+```
+
+It's recommended that you spread these properties into your ModuleFederationPlugin configuration, like so:
+
+#### `webpack.config.js`
+
+```javascript
+
+const deps = require('../package.json').dependencies;
+const federationConfig = require('./federation.config.json');
+
+module.exports = {
+    ...
+
+    plugins: [
+        new ModuleFederationPlugin({
+            ...federationConfig,
+            filename: "remoteEntry.js",
+            shared: {
+                ...deps,
+            },
+        }),
+    ],
+
+    ...
+}
+
+```
+
+Then you can call `make-federated-types` from your `scripts` block in your package's `package.json` file:
+
+#### `package.json`
+
+```json
+scripts: {
+    "make-types": "make-federated-types"
+}
+```
+
+This will write new package to the `node_modules/@types/__federated_types` in your project. Since Tyepscript will resolve typings in the `node_modules/@types` directory by default, you won't have to set up any other resolution or pathing in your tsconfig files to start using your typings.
+
+If you would rather specify a directory in which to write the typing files, you can pass an `--outputDir` parameter to the command like so:
+
+#### `package.json`
+
+```json
+scripts: {
+    "make-types": "make-federated-types --outputDir ../../my_types/"
+}
+```
---
+++ cli.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+
+const path = require('path');
+const fs = require('fs');
+const ts = require('typescript');
+
+const getArg = (argName) => {
+    const argIndex = process.argv.indexOf(argName);
+    return argIndex !== -1 ? process.argv[argIndex + 1] : null;
+};
+
+const outDirArg = getArg('--outputDir');
+const outputDir = outDirArg
+    ? path.resolve('./', outDirArg)
+    : path.resolve(__dirname, '../../@types/__federated_types/');
+
+const findFederationConfigs = (base, files, result) => {
+    files = files || fs.readdirSync(base);
+    result = result || [];
+
+    files.forEach((file) => {
+        const newBase = path.join(base, file);
+        if (fs.statSync(newBase).isDirectory()) {
+            result = findFederationConfigs(newBase, fs.readdirSync(newBase), result);
+        } else {
+            if (file === 'federation.config.json') {
+                result.push(require(path.resolve('./', newBase)));
+            }
+        }
+    });
+
+    return result;
+};
+
+const configs = findFederationConfigs('./');
+
+if (configs.length !== 1) {
+    console.error(`ERROR: Found ${configs.length} federation configs`);
+    process.exit(1);
+}
+
+const federationConfig = configs[0];
+const compileFiles = Object.values(federationConfig.exposes);
+const outFile = path.resolve(outputDir, `${federationConfig.name}.d.ts`);
+
+try {
+    // write the typings file
+    const program = ts.createProgram(compileFiles, {
+        outFile,
+        declaration: true,
+        emitDeclarationOnly: true,
+        skipLibCheck: true,
+        jsx: 'react',
+        esModuleInterop: true,
+    });
+
+    program.emit();
+
+    typing = fs.readFileSync(outFile, { encoding: 'utf8', flag: 'r' });
+
+    console.log('writing typing file:', outFile);
+
+    fs.writeFileSync(
+        outFile,
+        typing.replace(/declare module \"/g, `declare module "${federationConfig.name}/`)
+    );
+
+    // if we are writing to the node_modules/@types directory, add a package.json file
+    if (outputDir.includes('node_modules/@types')) {
+        const packageJsonPath = path.resolve(outputDir, 'package.json');
+
+        if (!fs.existsSync(packageJsonPath)) {
+            console.log('writing package.json:', packageJsonPath);
+            fs.copyFileSync(path.resolve(__dirname, 'typings.package.tmpl.json'), packageJsonPath);
+        } else {
+            console.log(packageJsonPath, 'already exists');
+        }
+    } else {
+        console.log('not writing to node modules, dont need a package.json');
+    }
+
+    // write/update the index.d.ts file
+    const indexPath = path.resolve(outputDir, 'index.d.ts');
+    const importStatement = `export * from './${federationConfig.name}';`;
+
+    if (!fs.existsSync(indexPath)) {
+        console.log('creating index.d.ts file');
+        fs.writeFileSync(indexPath, `${importStatement}\n`);
+    } else {
+        console.log('updating index.d.ts file');
+        const contents = fs.readFileSync(indexPath);
+        if (!contents.includes(importStatement)) {
+            fs.writeFileSync(indexPath, `${contents}${importStatement}\n`);
+        }
+    }
+
+    console.log('Success!');
+} catch (e) {
+    console.error(`ERROR:`, e);
+    process.exit(1);
+}
